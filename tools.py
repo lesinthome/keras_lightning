05dad194bab6c55e1f1979bd09d95df8e960781d
solo_machine_learning <sirawit.k@clicknic.co>
2025-01-29 13:03:46 +0700

    edit some lines

--- tools.py
+++ tools.py
@@ -2,13 +2,14 @@ import pytorch_lightning as pl
 from keras import ops
 import torch
 
-def compile(model,optimizer,loss,metrics):
+def compile(model, optimizer, loss, metrics, device="cpu"):
   class LightningModel(pl.LightningModule):
-    def __init__(self, model,optimizer,loss,metrics):
+    def __init__(self, model, optimizer, loss, metrics):
         super().__init__()
         self.model = model
         self.optimizer = optimizer
         self.criterion = loss
+        self.device = device
         self.metrics_name = []
         self.metrics_func = []
         for i in range(len(metrics)):
@@ -20,8 +21,8 @@ def compile(model,optimizer,loss,metrics):
         return self.model(x)
 
     def prepare_input(self, x, y):
-        x = ops.convert_to_tensor(x).type(torch.LongTensor).to(device)
-        y = ops.convert_to_tensor(y).type(torch.LongTensor).to(device)
+        x = ops.convert_to_tensor(x).type(torch.LongTensor).to(self.device)
+        y = ops.convert_to_tensor(y).type(torch.LongTensor).to(self.device)
         return x, y
 
     def training_step(self, batch, batch_idx):
@@ -31,8 +32,8 @@ def compile(model,optimizer,loss,metrics):
         loss = self.criterion(predictions, labels)
         self.log('train_loss', loss, prog_bar=True, logger=True)
 
-        for i in range(len(metrics_func)):
-            self.log(f'train_{metrics_name[i]}', self.metrics_func[i](predictions, labels), prog_bar=True, logger=True)
+        for i in range(len(self.metrics_func)):
+            self.log(f'train_{self.metrics_name[i]}', self.metrics_func[i](predictions, labels), prog_bar=True, logger=True)
         return loss
 
     def configure_optimizers(self):
